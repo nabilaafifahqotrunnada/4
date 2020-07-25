ab83adc0c80906bc12fa2f374998b17faf747146
nabilaafifahqotrunnada <nabila_afifah_28rpl@student.smktelkom-mlg.sch.id>
2020-07-25 09:19:16 +0700

    Second Commit

---
+++ 4.js
@@ -0,0 +1,25 @@
+//array string
+let jurusan = ["Rekayasa Perangkat Lunak","Teknik Komputer dan Jaringan"]
+
+//array numeric
+let tingkatan_kelas = [10,11,12]
+
+//array objek
+let siswa = [
+    //objek 1
+    {
+        nama: "Yaya", jurusan: "RPL"
+    },
+    //objek 2
+    {
+        nama: "Ying", jurusan: "TKJ"
+    },
+    //objek 3
+    {
+        nama: "Gopal", jurusan: "RPL"
+    }
+]
+
+console.log("Jumlah elemen array jurusan = " + jurusan.length)
+console.log("Jumlah elemen array tingkatan kelas = " + tingkatan_kelas.length)
+console.log("Jumlah elemen array siswa = " + siswa.length)
